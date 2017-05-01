15cf0e162a76e5f69fa65b23f7e49ca80f71a2de
Kamil Lewan <carmel4a@o2.pl>
2017-05-02 01:40:51 +0200

    River and fix

--- Level/Level.gd
+++ Level/Level.gd
@@ -20,14 +20,15 @@ func _ready():
 			if y != 0:
 				_as.connect_points(_p,\
 					_p-map_size.x)
-			
 			grass(x,y)
 			tree(x,y)
-
+	river()
+	river()
+	
 	Global.build_a_house(floor(map_size.x/2),floor(map_size.y/2+1))
 	Global.build_a_house(floor(map_size.x/2),floor(map_size.y/2-1))
-	Global.add_cell([[floor(map_size.x/2),floor(map_size.y/2),"Roads",6]])
-	Global.adding_to_map("Water",0)
+	LevelState.add_cell([[floor(map_size.x/2),floor(map_size.y/2),"Roads",6]])
+	LevelState.adding_to_map("Water",0)
 
 func _add(c):
 	
@@ -74,3 +75,28 @@ func tree(x,y):
 			LevelState.add_cell([[x-1,y+1,"Trees",0]])
 		if randf() > 0.5:
 			LevelState.add_cell([[x-1,y-1,"Trees",0]])
+
+func river():
+	var _s1 = randi()%4
+	var _sl = Vector2()
+	var _el = Vector2()
+	var _ar = [_sl,_el]
+	for i in range(_ar.size()):
+		var _s2 = randi()%4
+		while _s1 == _s2:
+			_s2 = randi()%4
+
+		_s1 = _s2
+		if _s2 == 0:
+			_ar[i] = Vector2(0,rand_range(0,map_size.y)).floor()
+		if _s2 == 1:
+			_ar[i] = Vector2(rand_range(0,map_size.x),0).floor()
+		if _s2 == 2:
+			_ar[i] = Vector2(map_size.x-1,rand_range(0,map_size.y)).floor()
+		if _s2 == 3:
+			_ar[i] = Vector2(rand_range(0,map_size.x),map_size.y-1).floor()
+	LevelState.add_cell([[_ar[0].x,_ar[0].y,"Water",0]])
+	LevelState.add_cell([[_ar[1].x,_ar[1].y,"Water",0]])
+	for i in _as.get_point_path(_ar[0].y*map_size.x+_ar[0].x,_ar[1].y*map_size.x+_ar[1].x):
+		LevelState.add_cell([[i.x,i.y,"Water",0]])
+	
