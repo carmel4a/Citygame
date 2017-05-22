29ff401136cd5efbc7bd8accff70902879828463
Kamil Lewan <carmel4a97@gmail.com>
2017-05-22 23:57:31 +0200

    Tooltip refactor

--- Tooltip/Tooltip.gd
+++ Tooltip/Tooltip.gd
@@ -3,19 +3,20 @@ extends Control
 onready var RTL = get_node("RTLabel")
 
 func set_text(s):
-	get_node("RTLabel").set_bbcode(s)
-	get_node("RTLabel").update()
+	clear()
+	RTL.set_size(Vector2(300,0))
+	RTL.set_bbcode(s)
+	update()
 
 func clear():
-	get_node("RTLabel").clear()
-	get_node("RTLabel").update()
+	RTL.clear()
+	update()
 
-func preupdate():
-	get_node("RTLabel").set_size(Vector2(300,0))
-#	get_node("RTLabel").set_size(Vector2(get_node("RTLabel").get_size().x,0))
-	
+
+func _ready():
+	update()
+
 func update():
-	get_node("RTLabel")._first = false
-#	set_size(Vector2(0,0))
-	get_node("RTLabel").update()
-	
+	yield(get_tree(), "idle_frame")
+	RTL.set_size(Vector2(RTL.get_size().width, RTL.get_v_scroll().get_max()))
+	set_size(RTL.get_size())
