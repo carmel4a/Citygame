4281d64e197fe806b9a8b366faf2e9b13df2f3f6
Kamil Lewan <carmel4a97@gmail.com>
2017-05-24 00:04:51 +0200

    Fix Tooltip

--- Tooltip/Tooltip.gd
+++ Tooltip/Tooltip.gd
@@ -1,9 +1,10 @@
 extends Control
 
 onready var RTL = get_node("RTLabel")
-
+var width = 120 setget set_width
 func set_text(s):
 	clear()
+	yield(get_tree(),"idle_frame")
 	RTL.set_bbcode(s)
 	update()
 
@@ -12,10 +13,14 @@ func clear():
 	update()
 
 func _ready():
-	RTL.set_size(Vector2(120,0))
-	
-
+	RTL.set_size(Vector2(width,0))
+	clear()
+	update()
 func update():
 	yield(get_tree(), "idle_frame")
-	RTL.set_size(Vector2(120, RTL.get_v_scroll().get_max()))
+	RTL.set_size(Vector2(width, RTL.get_v_scroll().get_max()))
 	set_size(RTL.get_size())
+
+func set_width(w):
+	width = w
+	update()
